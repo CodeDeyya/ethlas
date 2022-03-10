d13ed6bd296085b50a79bc7a935f8f513a9d21ba
CodeDeyya <CodeDeyya@gmail.com>
2022-03-10 23:51:10 +0530

    added floating button

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -26,7 +26,29 @@ const Game = ({
   const gameScene = React.useCallback((width, height) => {
     var scene = new Phaser.Scene('game');
     let button;
+    let fauna;
     scene.preload = function () {};
+    let up = false;
+
+    class Bee extends Phaser.GameObjects.Image {
+      constructor(config) {
+        super(config.scene, config.x, config.y, 'button');
+        config.scene.add.existing(this);
+        config.scene.physics.add.existing(this);
+        this.x = config.x;
+        this.y = config.y;
+        this.body.allowGravity = false;
+        config.scene.tweens.add({
+          targets: this.body.velocity,
+          y: { from: -50, to: 50 },
+          ease: Phaser.Math.Easing.Quadratic.InOut,
+          yoyo: true,
+          repeat: -1,
+          duraton: 1000,
+          delay: 100,
+        });
+      }
+    }
 
     scene.create = function () {
       const map = this.make.tilemap({ key: 'dungeon' });
@@ -34,15 +56,59 @@ const Game = ({
       map.createLayer('Floor', tileset);
       map.createLayer('Flora', tileset);
       map.createLayer('Buildings', tileset);
-      button = this.add.image(390, 200, 'button');
-      button.setInteractive();
-      button.on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
+
+      fauna = this.add.sprite(300, 300, 'fauna', 'run-down-3.png');
+      this.anims.create({
+        key: 'faune-idle-down',
+        frames: [{ key: 'fauna', frame: 'walk-down-3.png' }],
+      });
+      this.anims.create({
+        key: 'faune-idle-up',
+        frames: [{ key: 'fauna', frame: 'walk-up-3.png' }],
+      });
+      this.anims.create({
+        key: 'faune-run-down',
+        frames: this.anims.generateFrameNames('fauna', {
+          start: 1,
+          end: 8,
+          prefix: 'run-down-',
+          suffix: '.png',
+        }),
+        repeat: -1,
+        frameRate: 60,
+      });
+      this.anims.create({
+        key: 'faune-run-up',
+        frames: this.anims.generateFrameNames('fauna', {
+          start: 1,
+          end: 8,
+          prefix: 'run-up-',
+          suffix: '.png',
+        }),
+        repeat: -1,
+        frameRate: 60,
+      });
+      this.anims.create({
+        key: 'faune-run-side',
+        frames: this.anims.generateFrameNames('fauna', {
+          start: 1,
+          end: 8,
+          prefix: 'run-side-',
+          suffix: '.png',
+        }),
+        repeat: -1,
+        frameRate: 60,
+      });
+      fauna.anims.play('faune-run-side');
+
+      let bee = new Bee({ scene: this, x: 400, y: 190 });
+      bee.setInteractive();
+      bee.on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
         setOpenMessage(true);
       });
     };
 
-    scene.update = function () {};
-
+    scene.update = function (t, dt) {};
     return scene;
   }, []);
 
@@ -53,6 +119,7 @@ const Game = ({
         this.load.image('tiles', 'tiles/atlas.png');
         this.load.tilemapTiledJSON('dungeon', 'assets/tiles/map.json');
         this.load.image('button', 'assets/ui/button.png');
+        this.load.atlas('fauna', 'assets/character/fauna.png');
       };
 
       scene.create = function () {
